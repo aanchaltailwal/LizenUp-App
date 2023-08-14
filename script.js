2d40b51f1e2fddec1f0643a79cbbb7d3b16882e0
aanchaltailwal <tailwalaanchal@gmmail.com>
2023-08-14 21:49:53 +0530

    changes commit

---
+++ script.js
@@ -0,0 +1,50 @@
+console.log("Welcome to Spotify");
+
+//initialize the variables
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+let myProgressbar = document.getElementById('myProgressbar');
+let gif = document.getElementById('gif');
+
+let songs =[
+    {songname: "Tujhe Kitna Chahne Lage", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songname: "Tujhe Kitna Chahne Lage", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songname: "Tujhe Kitna Chahne Lage", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songname: "Tujhe Kitna Chahne Lage", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songname: "Tujhe Kitna Chahne Lage", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songname: "Tujhe Kitna Chahne Lage", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songname: "Tujhe Kitna Chahne Lage", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songname: "Tujhe Kitna Chahne Lage", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+    {songname: "Tujhe Kitna Chahne Lage", filePath: "song/1.mp3", coverPath: "covers/1.jpg"},
+]
+
+//audioElement.play();
+
+
+//handle play/pause click
+masterPlay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
+        gif.style.opacity = 0;
+    }
+})
+
+//listen to events
+audioElement.addEventListener('timeupdate', ()=>{
+    //update Seekbar
+    progress = parseInt((audioElement.currentTime/audioElement.duration)* 100)
+    myProgressBar.value = progress;
+})
+
+myProgressBar.addEventListener('change', ()=>{
+    audioElement.currentTime = myProgressBar.value * audioElement.duration/100;
+})
